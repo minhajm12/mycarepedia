fe47c82b8085ab9b4210ba67a4faa87ed79c252b
minhajm12 <minhajatholi222@gmail.com>
2024-06-16 23:14:42 +0530

    Doctors Dashboard

---
+++ assets/js/patient_dashboard.js
@@ -0,0 +1,20 @@
+let menu_open = document.querySelector('.menuopen')
+let menucanvs = document.querySelector(".menucnvs")
+
+menu_open.addEventListener('click', function (event) {
+      menucanvs.classList.toggle('menucnvs-active')
+      event.stopPropagation();
+})
+
+// Close Menu
+let menu_close = document.querySelector('.menuclose')
+menu_close.addEventListener('click', function () {
+      document.querySelector(".menucnvs").classList.remove('menucnvs-active')
+})
+
+document.addEventListener("click", function(event) {
+      // Check if the clicked element is outside of the dropdown div
+      if (!menucanvs.contains(event.target) && event.target !== menu_open) {
+          menucanvs.classList.remove('menucnvs-active'); // Hide the dropdown div
+      }
+  });
